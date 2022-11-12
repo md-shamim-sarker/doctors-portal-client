0bd34ed63c7a1ebabf466e782f4a71ae1967e7c1
shamimspro <shamimspro@gmail.com>
2022-11-12 09:02:24 +0600

    create src file folder

---
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MakeAppointment = () => {
+    return (
+        <div>
+            <h2>MakeAppointment</h2>
+        </div>
+    );
+};
+
+export default MakeAppointment;
---
+++ src/components/PrimaryButton/PrimaryButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrimaryButton = () => {
+    return (
+        <div>
+            <h2>PrimaryButton components</h2>
+        </div>
+    );
+};
+
+export default PrimaryButton;
